747ff96ec731bcbacdb29a695a3bcd3337291c9c
fazizsoltani <farzad.azizsoltani98@gmail.com>
2024-01-28 21:00:09 +0100

    update workflow file

---
+++ example/renovate-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: true,
+  platform: 'github',
+  includeForks: true,
+  repositories: ['fazizsoltani/renovate-test'],
+
+};
